2c02d98adc6d420a73b14f2c8e52b1171e9502bb
admin <admin@MacBook-Pro-admin.local>
2019-06-16 04:04:38 +0300

    доделал логику изменения количества опций корины, корректное поведение при удалении, которое не только на иконку, но и при изменении количества товара на ноль, также идет рассчет стоимости опции с учетов его количества

--- src/pages/Basket/Basket.js
+++ src/pages/Basket/Basket.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Table } from 'antd';
+import { Table, Icon } from 'antd';
 
-import { DELETE_PRODUCT_IN_BASKET } from 'store/actionTypes';
+import { DELETE_PRODUCT_FROM_BASKET, CHANGE_PRODUCT_IN_BASKET } from 'store/actionTypes';
 import { PageTitle } from 'components/PageTitle/PageTitle';
 import { EmptyComponent } from 'components/EmptyComponent/EmptyComponent';
 
@@ -11,24 +12,36 @@ import styles from './Basket.less';
 
 const { Column } = Table;
 
-const Basket = ({ basket, changeBasket }) => {
+const Basket = ({ basket, deleteProduct, changeProduct }) => {
+  const renderDeleteIcon = (product) => (
+    <div onClick={() => deleteProduct(product)}>
+      <Icon type="delete" />
+    </div>
+  );
+
+  const renderCount = (product) => (
+    <div className={styles.countContainer}>
+      <div className={styles.countIconMinus} onClick={() => changeProduct({ ...product, count: product.count - 1 })}>
+        <Icon type="minus" />
+      </div>
+      <h3 className={styles.count}>{product.count}</h3>
+      <div className={styles.countIconPlus} onClick={() => changeProduct({ ...product, count: product.count + 1 })}>
+        <Icon type="plus" />
+      </div>
+    </div>
+  );
+
+
   const renderBasket = () => (
     <Fragment>
       <PageTitle title='Корзина' />
       <div className={styles.basketContainer}>
-        <Table dataSource={basket} pagination={false}>
-          <Column title="Наименование" dataIndex="product" key="product" />
-          <Column title="Количество" dataIndex="count" key="count" />
-          <Column title="Сумма" dataIndex="price" key="price" />
-          <Column
-            title="Action"
-            key="action"
-            render={(product) => (
-              <div onClick={() => changeBasket(product)}>
-                 Delete
-              </div>
-            )}
-          />
+        <Table dataSource={basket} pagination={false} rowKey={(product) => product.id}>
+          <Column title="Наименование" key="product" dataIndex="product" />
+          <Column title="Описание" key="description" dataIndex="description"/>
+          <Column align="center" title="Количество" key="count" render={(product) => renderCount(product)}/>
+          <Column align="center" title="Сумма" key="price" render={({ price, count }) => price * count} />
+          <Column align="center" key="action" render={(product) => renderDeleteIcon(product)} />
         </Table>
       </div>
     </Fragment>
@@ -44,7 +57,8 @@ const Basket = ({ basket, changeBasket }) => {
 
 Basket.propTypes = {
   basket: PropTypes.array,
-  changeBasket: PropTypes.func,
+  deleteProduct: PropTypes.func,
+  changeProduct: PropTypes.func,
 };
 
 
@@ -56,7 +70,8 @@ const mapStateToProps = ({ basket }) => (
 
 const mapDispatchToProps = (dispatch) => (
   {
-    changeBasket: (product) => dispatch({ type: DELETE_PRODUCT_IN_BASKET, payload: product })
+    deleteProduct: (product) => dispatch({ type: DELETE_PRODUCT_FROM_BASKET, payload: product }),
+    changeProduct: (product) => dispatch({ type: CHANGE_PRODUCT_IN_BASKET, payload: product })
   }
 );
 
--- src/store/reducers/basket.js
+++ src/store/reducers/basket.js
@@ -1,20 +1,45 @@
-import { PUT_PRODUCT_TO_BASKET, DELETE_PRODUCT_IN_BASKET } from '../actionTypes';
+import {
+  PUT_PRODUCT_TO_BASKET,
+  DELETE_PRODUCT_FROM_BASKET,
+  CHANGE_PRODUCT_IN_BASKET,
+} from '../actionTypes';
 
 const basket = (state = [], action) => {
+  const payloadID = action.payload ? action.payload.id : undefined;
+  const findFunc = product => product.id === payloadID;
+  const findIndexExistProduct = state.findIndex(findFunc);
+  const isExist = findIndexExistProduct !== -1;
+  const filterFunc = product => product.id !== payloadID;
+  // const mapFunc = product => (
+  //   product.id !== payloadID ? product : action.payload
+  // );
+
+  const reduceFunc = (accum, current) => {
+    if (current.id !== payloadID) {
+      return [...accum, current];
+    }
+    if (action.payload.count > 0) {
+      return [...accum, action.payload];
+    }
+    return accum;
+  };
+
   switch (action.type) {
     case PUT_PRODUCT_TO_BASKET: {
-      const findFunc = product => product.id === action.payload.id;
-      const findIndexExistProduct = state.findIndex(findFunc);
-      const isExist = findIndexExistProduct !== -1;
       const count = isExist ? state[findIndexExistProduct].count + 1 : 1;
       const payload = { ...action.payload, count };
-      const filterFunc = product => product.id !== action.payload.id;
-
       return [...state.filter(filterFunc), payload];
     }
-    case DELETE_PRODUCT_IN_BASKET: {
-      return [...state.filter(product => product.id !== action.payload.id)];
+
+    case DELETE_PRODUCT_FROM_BASKET: {
+      return [...state.filter(filterFunc)];
     }
+
+    case CHANGE_PRODUCT_IN_BASKET: {
+      // return [...state.map(mapFunc)];
+      return [...state.reduce(reduceFunc, [])];
+    }
+
     default:
       return state;
   }
