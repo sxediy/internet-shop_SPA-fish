41343d499311fbf6aca72c7f04176ce933d376ef
admin <admin@MacBook-Pro-admin.local>
2019-06-16 05:10:17 +0300

    Футер корзины с общей суммой и кнопка Checkout

--- src/pages/Basket/Basket.js
+++ src/pages/Basket/Basket.js
@@ -1,11 +1,14 @@
-/* eslint-disable no-unused-vars */
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Table, Icon } from 'antd';
+import {
+  Table,
+  Icon,
+  Button,
+  Progress
+} from 'antd';
 
 import { DELETE_PRODUCT_FROM_BASKET, CHANGE_PRODUCT_IN_BASKET } from 'store/actionTypes';
-import { PageTitle } from 'components/PageTitle/PageTitle';
 import { EmptyComponent } from 'components/EmptyComponent/EmptyComponent';
 
 import styles from './Basket.less';
@@ -31,18 +34,44 @@ const Basket = ({ basket, deleteProduct, changeProduct }) => {
     </div>
   );
 
+  const renderFooter = () => {
+    const summaryPrice = basket.reduce((accum, current) => accum + current.price * current.count, 0);
+    return (
+      <div className={styles.basketFooter}>
+        <h1>Итого:</h1>
+        <h1>{`${summaryPrice} р.`}</h1>
+      </div>
+    );
+  };
+
 
   const renderBasket = () => (
     <Fragment>
-      <PageTitle title='Корзина' />
       <div className={styles.basketContainer}>
-        <Table dataSource={basket} pagination={false} rowKey={(product) => product.id}>
-          <Column title="Наименование" key="product" dataIndex="product" />
-          <Column title="Описание" key="description" dataIndex="description"/>
+        <Table
+          title={() => <h2>Корзина покупок</h2>}
+          dataSource={basket}
+          pagination={false}
+          rowKey={(product) => product.id}
+          footer={renderFooter}
+        >
+          <Column title="Наименование" key="product" dataIndex="product" width="200px"/>
+          <Column title="Описание" key="description" dataIndex="description" width="600px"/>
           <Column align="center" title="Количество" key="count" render={(product) => renderCount(product)}/>
           <Column align="center" title="Сумма" key="price" render={({ price, count }) => price * count} />
           <Column align="center" key="action" render={(product) => renderDeleteIcon(product)} />
         </Table>
+        <div className={styles.basketProgress}>
+          <Progress percent={100} size="small" strokeColor="#84aff4" status="success" />
+        </div>
+        <Button
+          type="ghost"
+          shape="round"
+          size="large"
+          onClick={() => {}}
+        >
+          <h3>Checkout</h3>
+        </Button>
       </div>
     </Fragment>
   );
