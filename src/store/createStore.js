03d8dc0ce55834afbf87449ba9ef92f10a3e8706
admin <admin@MacBook-Pro-admin.local>
2019-06-20 18:56:59 +0300

    Local Storage для сохранения стостояния приложения после перезагрузки страницы / закрытия вкладки

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -2,16 +2,26 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootreducer from './reducers';
 import sagas from './sagas';
+import { loadState, saveState } from './localStorage';
 
+const initialState = loadState();
 const sagaMiddleware = createSagaMiddleware();
 
 const createStoreWithMiddleware = compose(
   applyMiddleware(sagaMiddleware)
 )(createStore);
 
-const store = createStoreWithMiddleware(rootreducer, {},
+const store = createStoreWithMiddleware(rootreducer, initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState({
+    products: store.getState().products,
+    basket: store.getState().basket,
+    order: store.getState().order,
+  });
+});
 
 sagaMiddleware.run(sagas);
 
